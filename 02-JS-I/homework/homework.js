1cb2e64430e6cb81f17a740c3548ac761d416809
Jose Chavarria <josechavarria@me.com>
2021-12-27 11:38:24 +0100

    final homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -262,13 +262,9 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   
-  if ( letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u' ) {  
-    return "Es vocal" 
-  }    
-    if ( letra.length > 1 ) ; {
-    return "Dato incorrecto" 
-  } 
-  
+   if ( letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u' ) return 'Es vocal';
+   if (letra.length > 1 ) return 'Dato incorrecto' ;
+   return 'Dato incorrecto' ;  
     
 }
 
